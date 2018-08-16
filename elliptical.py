ef68450954eab9c6c815557ba47213ea2a8e348f
Jack Jefferson <eptgen@gmail.com>
2018-08-15 17:15:39 -0700

    began trying for n-degree precision

--- elliptical.py
+++ elliptical.py
@@ -1,41 +1,76 @@
-from scipy.sparse import lil_matrix
+from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import spsolve
 from math import sin
 import numpy as np
 from matplotlib.pyplot import contour, show
 
-def gen_elliptical_square(h):
-	m = int((1 - h) / h)
-	result = lil_matrix((m**2, m**2))
+def gen_elliptical_cube(h):
+	pass
+
+def gen_elliptical_square(h, order):
+	m = (round((1 - h[0]) / h[0]), round((1 - h[1]) / h[1]))
+	ai = []
+	aj = []
+	av = []
 	def imap(i, j):
-		return m * i + j
-#	3for i in range(m + 1):
+		return m[1] * i + j
+#	for i in range(m + 1):
 #	result.set(imap(0, i), imap(0, i), 1)
-	for i in range(m):
-		for j in range(m):
-			if i==0 or i==m-1 or j==0 or j==m-1:
-				result[imap(i, j), imap(i, j)] = 1
+	for i in range(m[0]):
+		for j in range(m[1]):
+			inds = (i, j)
+			if i == 0 or i == m[0] - 1 or j == 0 or j == m[1] - 1:
+				ai.append(imap(i, j))
+				aj.append(imap(i, j))
+				av.append(1)
 			else:
-				result[imap(i, j), imap(i, j)] = -4 * h**-2
-				result[imap(i, j), imap(i - 1, j)] = h**-2
-				result[imap(i, j), imap(i + 1, j)] = h**-2
-				result[imap(i, j), imap(i, j - 1)] = h**-2
-				result[imap(i, j), imap(i, j + 1)] = h**-2
+				ai.append(imap(i, j))
+				aj.append(imap(i, j))
+				av.append(-2 * h[0]**-2 - 2 * h[1]**-2)
+				ai.append(imap(i, j))
+				aj.append(imap(i - 1, j))
+				av.append(h[0]**-2)
+				ai.append(imap(i, j))
+				aj.append(imap(i + 1, j))
+				av.append(h[0]**-2)
+				ai.append(imap(i, j))
+				aj.append(imap(i, j - 1))
+				av.append(h[1]**-2)
+				ai.append(imap(i, j))
+				aj.append(imap(i, j + 1))
+				av.append(h[1]**-2)
+				for k in range(2):
+					offcenter = 0
+					if inds[k] < order / 2 - 1:
+						offcenter = inds[k] - order / 2
+					elif (m[k] - 1) - inds[k] < order / 2 - 1:
+						offcenter = order / 2 - ((m[k] - 1) - inds[k])
+					weights = finite_diff(order, offcenter)
+					for l in range(len(weights)):
+						weight = weights[l]
+						
+				
 	#for i in range(m + 1):
 	#	result.set(imap(m + 1, i), imap(m + 1, i), 1)
-	return result
+	#print(ai)
+	#print(aj)
+	#print(av)
+	return coo_matrix((av, (ai, aj)), (m[0] * m[1], m[0] * m[1]))
 
 shapes = {
-	"square": gen_elliptical_square
+	"square": gen_elliptical_square,
+	"cube": gen_elliptical_cube
 }
 
-def gen_elliptical(shape, h):
+def gen_elliptical(shape, h, order):
 	return shapes[shape](h)
 	
-m = 50
-h = 1 / (m + 1)
-A = gen_elliptical("square", h)
-print(A)
+m1 = 100
+h1 = 1 / (m1 + 1)
+m2 = 50
+h2 = 1 / (m2 + 1)
+A = gen_elliptical("square", (h1, h2), 2)
+# print(A)
 print()
 # lst = res.to_dense(m**2)
 """
@@ -50,9 +85,9 @@ for i in lst:
 f = lambda x, y: sin(x) * y ** 2 + x
 f_lst = []
 
-for i in range(m):
-	for j in range(m):
-		if (m / 8 < i and 7 * m / 8 > i and m / 8 < j and m / 4 > j) or (m / 8 < i and m / 4 > i and m / 8 < j and 7 * m / 8 > j):
+for i in range(m1):
+	for j in range(m2):
+		if (m1 / 8 < i and 7 * m1 / 8 > i and m2 / 8 < j and m2 / 4 > j) or (m1 / 8 < i and m1 / 4 > i and m2 / 8 < j and 7 * m2 / 8 > j):
 			f_lst.append(1)
 		else:
 			f_lst.append(0)
@@ -66,10 +101,10 @@ u = spsolve(A, f_lst)
 # print(u)
 
 pretty_u = []
-for i in range(m):
+for i in range(m1):
 	pretty_u.append([])
-	for j in range(m):
-		pretty_u[i].append(u[i * m + j])
+	for j in range(m2):
+		pretty_u[i].append(u[i * m2 + j])
 		
 contour(pretty_u)
 show()
